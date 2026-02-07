396e1d94e4d981a34b05bde32d731655fe912b64
Isidro Renzo D. De Jesus II <rnzodjsus@gmail.com>
2026-02-07 17:32:36 +0800

    math program

--- Program.cs
+++ Program.cs
@@ -4,30 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace math_class
+namespace math_program
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 5 gredes seperated by new line: ");
+            int a = 20;
+            int b = 4;
 
-            double[] grades = new double[5];
-            double sum = 0;
-
-            for (int i = 0; i < grades.Length; i++)
+            Console.WriteLine($"Addtion of {a} and {b}: {Add(a, b)}");
+            Console.WriteLine($"Subtraction of {a} and {b}: {Subtract(a, b)}");
+            Console.WriteLine($"Multiplication of {a} and {b}: {Multiply(a, b)}");
+            Console.WriteLine($"Division od {a} and {b}: {Divide(a, b)}");
+            Console.WriteLine($"Power of {a} and {b}: {Power(a, b)}");
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        static double Divide(int x, int y)
+        {
+            if (y == 0)
             {
-                grades[i] = Convert.ToDouble(Console.ReadLine());
-                sum += grades[i];
+                Console.WriteLine("Cannot divide by zero.");
+                return double.NaN; 
             }
-            double average = sum / grades.Length;
+            return (double)x / y;
+        }
+        static double Power(int baseNumber, int exponent)
+        {
+            return Math.Pow(baseNumber, exponent);
 
-            double roundedAverage = Math.Round(average);
-
-            Console.WriteLine("The average of the grades is: " + average);
-            Console.WriteLine("The rounded average of the grades is: " + roundedAverage);
-            Console.WriteLine("press any key to exit...");
             Console.ReadKey();
+
         }
     }
 }
